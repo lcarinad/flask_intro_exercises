6a4cfd0156ca0ff1c35fa3dcaa44d51c659ecfca
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-09-12 18:30:59 -0400

    add functionality for calc

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,16 @@
+from flask import Flask, request
+from operations import add
+app = Flask(__name__)
 # Put your app in here.
+
+
+
+
+@app.route("/add")
+def add_nums():
+    """Adds two numbers, a&b in query str"""
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    # return f"a={a} b={b}"
+    result = add(a,b)
+    return str(result)
